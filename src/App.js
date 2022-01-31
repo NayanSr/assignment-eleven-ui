7046a142135341ae178f9297a00c2523418cc184
NayanSr <nayankbps@gmail.com>
2022-01-31 09:54:26 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Tours from './pages/Home/Tours/Tours';
+import Booking from './pages/Booking/Booking';
+import Login from './pages/Login/Login/Login';
+import Header from './pages/Home/Header/Header';
+import AboutUs from './pages/AboutUs/AboutUs';
+import AuthProvider from './context/AuthProvider';
+import Privateroute from './pages/Login/Login/Privateroute/Privateroute';
+import AddService from './AddService/AddService';
+import ManageItems from './pages/ManageItems/ManageItems';
+import AllServices from './pages/AllServices/AllServices';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AuthProvider>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+            <Route path='/tours' element={<Tours />} />
+            <Route path='/tours/:tourId' element={<Booking />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/addservice' element={<AddService />} />
+            <Route path='/allservices' element={<AllServices />} />
+            <Route path='/managetour' element={<ManageItems />} />
+
+            <Route path='/about' element={<Privateroute>
+              <AboutUs />
+            </Privateroute>} />
+
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
 
 export default App;
+
+/*
+
+tour=[
+  {
+    id: 01,
+    name:'Sundarban',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price: 10000,
+    img: 'https://i.ibb.co/dD8Drsw/S-1.jpg'
+  },
+  {
+    id: 02,
+    name:'Bagerhat',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price:5000 ,
+    img: 'https://i.ibb.co/593dkth/S-2.jpg'
+  },
+  {
+    id: 3,
+    name:'Kuakata',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price: 8000,
+    img: 'https://i.ibb.co/84Jcfzt/S-3jpg.jpg'
+  },
+  {
+    id: 4,
+    name:'cox-Bazar',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price:12000,
+    img: 'https://i.ibb.co/Tg706yj/S-4.jpg'
+  },
+  {
+    id: 5,
+    name:'Sajeak',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price:10000,
+    img: 'https://i.ibb.co/XkxkjzZ/S-5.jpg'
+  },
+  {
+    id: 6,
+    name:'Nilgiri',
+    details: 'Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban Sundarban',
+    price:10000,
+    img: 'https://i.ibb.co/Dt01dbf/F-6.jpg'
+  }
+]
+
+*/
---
+++ src/pages/Login/Firebase/firebase.config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCOj3r_VpzGDSXOk7iBX5gujDFi4-oV29E",
+    authDomain: "tourism-assignment-98e66.firebaseapp.com",
+    projectId: "tourism-assignment-98e66",
+    storageBucket: "tourism-assignment-98e66.appspot.com",
+    messagingSenderId: "342410302850",
+    appId: "1:342410302850:web:56002ea0341c03ee7ceb9b"
+};
+
+
+export default firebaseConfig;
